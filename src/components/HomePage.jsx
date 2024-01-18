542fe779795b92d6f8bb81c335712c645c136648
kumarprince1407 <kumarprince1407@gmail.com>
2024-01-18 11:37:23 +0530

    made some minor modification on EditPage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,7 +1,7 @@
 //HomePage.jsx
 import axios from "axios";
 import React, { useState, useEffect } from "react";
-//import EditPage from "../components/EditPage";
+import EditPage from "../components/EditPage";
 import "./HomePage.css";
 import { Button, Checkbox } from "@mui/material";
 import { useNavigate } from "react-router-dom";
